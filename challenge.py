e984ac8643793a52e2b7aa940edb8cfc66791f28
Kingbanda <bandadesignske@gmail.com>
2025-08-06 15:29:47 +0300

    Readme added

--- challenge.py
+++ challenge.py
@@ -1,7 +1,7 @@
 def findNeedle(haystack):
     index = haystack.index("needle")
    # return index
-    return f"found the needle at position {index}"
+    return f"found the needle at position {index}"   
 
 haystack = ['junk', 'hay', 'more junk', 'shovel', 'needle', 'random stuff']
 print(findNeedle(haystack))
